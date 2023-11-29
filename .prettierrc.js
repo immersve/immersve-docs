da9222cf0b84916421d0d42b50de8c117bf8edc4
ptranny <96764636+ptranny@users.noreply.github.com>
2023-11-30 10:38:25 +1300

    chore(prettier-config): Change prettier config to double quotes for YAML (#158)
    The repo is currently using a mix of single and double quotes. We are
    standardising to double quotes for YAML.
    
    Ticket Link:
    https://immersve.slack.com/archives/C03471T0RMF/p1693456104685509?thread_ts=1693453548.763679&cid=C03471T0RMF

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,12 @@
 module.exports = {
   singleQuote: true,
+  trailingComma: 'es5',
+  overrides: [
+    {
+      files: ['*.yaml', '*.yml'],
+      options: {
+        singleQuote: false,
+      },
+    },
+  ],
 };
