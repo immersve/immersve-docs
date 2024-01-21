0a3c974f09d94107145ecb62c199f568f0de5c35
Nathan Jones <nathan@ncjones.com>
2024-01-22 02:35:02 +1300

    [WIP] add tailwind
    Tailwind config is from tailwind protocol template.
    
    TODO need to fix styling of starlight components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,25 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'My Docs',
+      title: 'Immersve Docs',
       social: {
-        github: 'https://github.com/immersve/immersve-docs',
+        github: 'https://github.com/immersve/immersve-docs'
       },
+      customCss: [
+        './src/styles/tailwind.css',
+      ],
+      components: {
+        MarkdownContent: './src/components/MarkdownContent.astro',
+      },
+    }), 
+    tailwind({
+      applyBaseStyles: false,
     }),
-  ],
+  ]
 });
