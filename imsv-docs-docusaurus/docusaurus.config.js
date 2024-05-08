779399d8fc6024bce8ac787a99b513daaebfdb2a
chanjant <jant.chan@immersve.com>
2024-05-08 16:34:13 +1200

    fix module not found

--- imsv-docs-docusaurus/docusaurus.config.js
+++ imsv-docs-docusaurus/docusaurus.config.js
@@ -1,8 +1,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require('prism-react-renderer').themes.github;
+const darkCodeTheme = require('prism-react-renderer').themes.dracula;
 
 async function createConfig() {
   return {
@@ -67,7 +67,7 @@ async function createConfig() {
               target: '_self',
             },
             {
-            label: 'Guides',
+              label: 'Guides',
               position: 'right',
               href: '//docs.immersve.com/guides/',
               target: '_self',
