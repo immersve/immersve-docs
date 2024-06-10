80b0d491823410b4053f8453c736c0e58c169f2a
Javier Durante <javier.durante@immersve.com>
2024-06-10 17:35:28 -0500

    Prepare Algorand protocol docs (#315)
    Preparing Algorand Flexi protocol public docs
    
    Ticket Link:
    https://www.notion.so/immersve/Public-docs-for-Flexi-Protocol-45f3cc65d9df4d2280f375e52606046f?pvs=4
    
    Depends on: #326  
    Depends on: #328
    
    ---------
    
    Co-authored-by: Nathan Jones <nathan.jones@immersve.com>
    Co-authored-by: BradenHolgate <47544289+braden-holgate@users.noreply.github.com>
    Co-authored-by: Nathan Jones <nathan@ncjones.com>

--- imsv-docs-astro/src/models/__tests__/models.test.mjs
+++ imsv-docs-astro/src/models/__tests__/models.test.mjs
@@ -83,16 +83,57 @@ describe('models', () => {
 
   describe('NetworkToken', () => {
 
-    test('fromContent', async () => {
-      const registry = await ContentRegistry.create();
-      const content = await getEntry('network-tokens', 'usdc-ethereum-sepolia');
-      const networkToken = NetworkToken.fromContent({ registry, content });
-      expect(networkToken.network).toEqual(registry.getNetwork('ethereum-sepolia'));
-      expect(networkToken.token).toEqual(registry.getToken('usdc'));
-      expect(networkToken.address).toEqual('0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238');
-      expect(networkToken.url).toEqual('https://sepolia.etherscan.io/address/0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238');
-      expect(networkToken.faucetTitle).toEqual('Circle USDC Faucet');
-      expect(networkToken.faucetUrl).toEqual('https://faucet.circle.com/');
+    describe('fromContent', async () => {
+
+      test('valid content', async () => {
+        const registry = await ContentRegistry.create();
+        const content = await getEntry('network-tokens', 'usdc-ethereum-sepolia');
+        const networkToken = NetworkToken.fromContent({ registry, content });
+        expect(networkToken.network).toEqual(registry.getNetwork('ethereum-sepolia'));
+        expect(networkToken.token).toEqual(registry.getToken('usdc'));
+        expect(networkToken.address).toEqual('0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238');
+        expect(networkToken.url).toEqual('https://sepolia.etherscan.io/address/0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238');
+        expect(networkToken.faucetTitle).toEqual('Circle USDC Faucet');
+        expect(networkToken.faucetUrl).toEqual('https://faucet.circle.com/');
+      });
+
+      test('fails when faucet configured on mainnet', async () => {
+        const registry = await ContentRegistry.create();
+        const content = {
+          id: 'usdc-polygon-mainnet',
+          data: {
+            address: '0x1234',
+            faucet: 'http://faucet',
+            faucetTitle: 'USDC Faucet',
+          }
+        };
+        expect(() => NetworkToken.fromContent({ registry, content })).toThrow('Faucet should not be defined on mainnet asset');
+      });
+
+      test('fails when faucet url missing on testnet', async () => {
+        const registry = await ContentRegistry.create();
+        const content = {
+          id: 'usdc-algorand-testnet',
+          data: {
+            address: 'ADDR1234',
+            faucetTitle: 'USDC Faucet',
+          }
+        };
+        expect(() => NetworkToken.fromContent({ registry, content })).toThrow('Faucet url must be defined on testnet asset');
+      });
+
+      test('fails when faucet title missing on testnet', async () => {
+        const registry = await ContentRegistry.create();
+        const content = {
+          id: 'usdc-algorand-testnet',
+          data: {
+            address: 'ADDR1234',
+            faucet: 'http://faucet',
+          }
+        };
+        expect(() => NetworkToken.fromContent({ registry, content })).toThrow('Faucet title must be defined on testnet asset');
+      });
+
     });
 
   });
