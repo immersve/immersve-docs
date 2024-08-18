db37045e7d0f8447ed8da11fd76738d3e93a4c11
Nathan Jones <nathan@ncjones.com>
2024-08-18 20:39:48 +1200

    Increase main content width when TOC disabled

--- imsv-docs-astro/tailwind.config.ts
+++ imsv-docs-astro/tailwind.config.ts
@@ -45,7 +45,7 @@ export default {
         lg: '33rem',
         '2xl': '40rem',
         '3xl': '50rem',
-        '5xl': '66rem',
+        '5xl': '68rem',
       },
       opacity: {
         1: '0.01',
