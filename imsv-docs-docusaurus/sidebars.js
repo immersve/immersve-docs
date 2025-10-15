b4d06ff481d7ab472bc71aabce700bc334da62a0
Oscar Crowe <oscar.crowe@immersve.com>
2025-10-16 11:03:20 +1300

    move filtering logic

--- imsv-docs-docusaurus/sidebars.js
+++ imsv-docs-docusaurus/sidebars.js
@@ -1,36 +1,41 @@
 const openApiSidebar = require('./docs/sidebar');
 const webhookSidebar = require('./docs/webhook-topics/sidebar');
 
+function getSidebarItems(sidebar, excludeId) {
+  const items = Array.isArray(sidebar)
+    ? sidebar
+    : sidebar.default
+
+  // exclude the specified ID
+  return items.filter((item) => item.id !== excludeId);
+}
+
 // @ts-check
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
   primarySidebar: [
       {
-      type: 'doc',
-      id: 'home/home',
+        type: 'doc',
+        id: 'home/home',
       },
-        {
-          type: 'category',
-          label: 'Endpoints',
-          link: {
-            type: 'generated-index',
-          },
-          collapsed: false,
-                    items: Array.isArray(openApiSidebar)
-            ? openApiSidebar.filter(item => item.id !== 'immersve-api')
-            : openApiSidebar.default.filter(item => item.id !== 'immersve-api')
+      {
+        type: 'category',
+        label: 'Endpoints',
+        link: {
+          type: 'generated-index',
         },
-        {
-          type: 'category',
-          label: 'Webhook Topics',
-          link: {
-            type: 'generated-index',
-          },
-          collapsed: false,
-          items: Array.isArray(webhookSidebar)
-            ? webhookSidebar.filter(item => item.id !== 'webhook-topics/immersve-webhooks')
-            : webhookSidebar.default.filter(item => item.id !== 'webhook-topics/immersve-webhooks')
+        collapsed: false,
+        items: getSidebarItems(openApiSidebar, 'immersve-api')
+      },
+      {
+        type: 'category',
+        label: 'Webhook Topics',
+        link: {
+          type: 'generated-index',
         },
+        collapsed: false,
+        items: getSidebarItems(webhookSidebar, 'webhook-topics/immersve-webhooks'),
+      },
   ],
 };
 
