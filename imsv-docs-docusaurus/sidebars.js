3efa02696727dfccda46f0f59b850c52a23f5139
Oscar Crowe <139089439+oscar-crowe22@users.noreply.github.com>
2025-10-16 12:22:53 +1300

    Add high level menu page for API's + Webhook Topics (#734)
    Add high level menu page for API's + Webhook Topics
    
    Ticket Link:
    
    https://www.notion.so/immersve/Include-webhook-topics-link-on-the-api-ref-landing-page-2841d446ed8a806f939ce54cad1d2227?v=1061d446ed8a80fc9bc6000cd77f6d88&source=copy_link

--- imsv-docs-docusaurus/sidebars.js
+++ imsv-docs-docusaurus/sidebars.js
@@ -1,34 +1,41 @@
 const openApiSidebar = require('./docs/sidebar');
 const webhookSidebar = require('./docs/webhook-topics/sidebar');
 
+function getSidebarItems(sidebar, excludeId) {
+  const items = Array.isArray(sidebar)
+    ? sidebar
+    : sidebar.default
+
+  // exclude auto-generated "introduction"
+  return items.filter((item) => item.id !== excludeId);
+}
+
 // @ts-check
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
   primarySidebar: [
-    {
-      type: 'category',
-      label: 'API Endpoints',
-      link: {
-        type: 'generated-index',
+      {
+        type: 'doc',
+        id: 'home/home',
       },
-      collapsed: false,
-      items: openApiSidebar.filter(
-        // exclude auto-generated "introduction"
-        (item) => item.id != 'immersve-api'
-      ),
-    },
-    {
-      type: 'category',
-      label: 'Webhook Topics',
-      link: {
-        type: 'generated-index',
+      {
+        type: 'category',
+        label: 'Endpoints',
+        link: {
+          type: 'generated-index',
+        },
+        collapsed: false,
+        items: getSidebarItems(openApiSidebar, 'immersve-api')
       },
-      collapsed: false,
-      items: webhookSidebar.filter(
-        // exclude auto-generated "introduction"
-        (item) => item.id != 'webhook-topics/immersve-webhooks'
-      ),
-    }
+      {
+        type: 'category',
+        label: 'Webhook Topics',
+        link: {
+          type: 'generated-index',
+        },
+        collapsed: false,
+        items: getSidebarItems(webhookSidebar, 'webhook-topics/immersve-webhooks'),
+      },
   ],
 };
 
