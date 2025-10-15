71da7f30ceed9c6c8bc1d75b820c39934c5c54eb
Oscar Crowe <oscar.crowe@immersve.com>
2025-10-15 15:55:14 +1300

    landing paghe

--- imsv-docs-docusaurus/sidebars.js
+++ imsv-docs-docusaurus/sidebars.js
@@ -5,30 +5,28 @@ const webhookSidebar = require('./docs/webhook-topics/sidebar');
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
   primarySidebar: [
-    {
-      type: 'category',
-      label: 'Endpoints',
-      link: {
-        type: 'generated-index',
+      {
+      type: 'doc',
+      id: 'redirects/home',
       },
-      collapsed: false,
-      items: openApiSidebar.filter(
-        // exclude auto-generated "introduction"
-        (item) => item.id != 'immersve-api'
-      ),
-    },
-    {
-      type: 'category',
-      label: 'Webhook Topics',
-      link: {
-        type: 'generated-index',
-      },
-      collapsed: false,
-      items: webhookSidebar.filter(
-        // exclude auto-generated "introduction"
-        (item) => item.id != 'webhook-topics/immersve-webhooks'
-      ),
-    },
+        {
+          type: 'category',
+          label: 'Endpoints',
+          link: {
+            type: 'generated-index',
+          },
+          collapsed: false,
+          items: openApiSidebar
+        },
+        {
+          type: 'category',
+          label: 'Webhook Topics',
+          link: {
+            type: 'generated-index',
+          },
+          collapsed: false,
+          items: webhookSidebar
+        },
   ],
 };
 
