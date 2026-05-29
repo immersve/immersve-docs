cf7d9eb26ea198100357ddf38823fb2200a39f41
Denis Tereshchenko <denis.tereschenko@immersve.com>
2026-05-29 13:18:14 +1200

    feat(Blocks): create a guide for cardholder activation

--- imsv-docs-astro/astro.config.mjs
+++ imsv-docs-astro/astro.config.mjs
@@ -40,6 +40,7 @@ export default defineConfig({
               { label: 'Immersve Conducted KYC', link: 'guides/immersve-conducted-kyc' },
               { label: 'Detecting KYC Completion', link: 'guides/detecting-kyc-completion' },
               { label: 'KYC Spending Prerequisites', link: 'guides/kyc-spending-prerequisites' },
+              { label: 'Cardholder Activation', link: 'guides/cardholder-activation' },
             ]},
             { label: 'Supported Chains', autogenerate: { directory: 'guides/supported-chains' } },
             { label: 'Supported Tokens', autogenerate: { directory: 'guides/supported-tokens' } },
